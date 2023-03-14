ef78d3d66a48119b174ba3ebe9e1efe1a4f9b104
Ganensh Gore <ganeshgore555@gmail.com>
2023-03-15 02:34:37 +0530

    Service Discovery

--- SpringServiceDiscovery/SpringFastpassUI/src/main/java/com/main/FastPassUIApplication.java
+++ SpringServiceDiscovery/SpringFastpassUI/src/main/java/com/main/FastPassUIApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.*")
 public class FastPassUIApplication {
 
 	public static void main(String[] args) {
